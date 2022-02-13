333afbd6e49f287acde17127c6331d1ee5c5f87b
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2022-02-13 19:43:21 +0500

    Added a simple character controller

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -7,20 +7,40 @@ export(int) var jumpSpeed
 var velocity : Vector2 = Vector2.ZERO
 var direction : float
 
-var currentState = null
-enum States {IDLE, RUN, JUMP}
 
-func _process(delta):
+
+func _process(delta) -> void:
 	
-	set_state()
-
-
-func _physics_process(delta):
+	direction = (Input.get_action_strength("right") - Input.get_action_strength("left"))
 	
-	pass
-
-
-func set_state():
-	pass
 	
 
+func _physics_process(delta) -> void:
+	
+	
+	move()
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	apply_gravity()
+	jump()
+	
+func move() -> void:
+	
+	velocity.x = direction * speed
+	
+func apply_gravity() -> void:
+	
+	if !is_on_floor():
+		velocity.y += gravity
+	else:
+		velocity.y = 1
+
+func jump() -> void:
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -jumpSpeed
+	
+	
+	
+	
+	
