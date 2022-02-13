8e157569d2a1745f2dd7fd23d87eeb9d6ba97efa
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2022-02-13 21:32:33 +0500

    Created Bullet.gd

---
+++ src/Bullet/Bullet/Bullet.gd
@@ -0,0 +1,21 @@
+extends Area2D
+class_name Bullet
+
+export(int) var speed
+
+var direction : Vector2
+
+func _ready():
+	set_as_toplevel(true)
+	global_position = get_parent().global_position
+
+func _physics_process(delta):
+	
+	position += direction * speed
+
+func set_direction(_direction) -> void:
+	
+	direction = _direction
+	
+	var angleOfRotation = Vector2.RIGHT.angle_to(direction)
+	$ColorRect.set_rotation(angleOfRotation)
