4685716a34a14d471beb2903c2297bed818b959a
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2022-02-16 16:12:17 +0500

    Wrote basic code for state

--- src/Enemies/Drone/States/State.gd
+++ src/Enemies/Drone/States/State.gd
@@ -1,16 +1,17 @@
 extends Node
+class_name State
 
+var state_machine = null
+var player : KinematicBody2D = null
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func enter() -> void:
+	pass
 
+func logic(delta) -> void:
+	pass
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func physics_logic(delta) -> void:
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func get_transition() -> Node:
+	return null 
