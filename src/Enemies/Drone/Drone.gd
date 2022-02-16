fef8d929a22a9450ed504ebd92aaac89594c21cd
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2022-02-16 16:03:31 +0500

    Added delegators to state machine
    Added delegators to state machine to delegate processes to active state.

--- src/Enemies/Drone/Drone.gd
+++ src/Enemies/Drone/Drone.gd
@@ -12,3 +12,16 @@ var ledgeOffset : int = 20
 onready var fieldOfView : RayCast2D = $FieldOfView
 onready var player : KinematicBody2D = get_tree().get_root().find_node("Player", true, false)
 
+onready var states : Node = $States
+
+func _ready():
+	states.enter()
+
+func _process(delta):
+	states.logic(delta)
+
+func _physics_process(delta):
+	states.physics_logic(delta)
+	
+	move_and_slide(velocity, Vector2.UP)
+	
