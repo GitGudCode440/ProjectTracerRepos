6cfa4bce3d9b7166499d2931d7ea5df8a02f89f3
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2022-02-16 16:35:01 +0500

    Added base code for assigning variables
    Added base code for assigning variables to children states such as itself and the player.

--- src/Enemies/Drone/States/state_machine.gd
+++ src/Enemies/Drone/States/state_machine.gd
@@ -11,15 +11,22 @@ var current_state : Node = null
 
 
 func enter() -> void:
+	current_state = states.PATROL
 	
+	for child in get_children():
+		child.stateMachine = self
+		child.player = player
+	
+	current_state.enter()
 	
 	pass
 
 func logic(delta) -> void:
-	pass
+	current_state.logic(delta)
 
 func physics_logic(delta) -> void:
-	pass
-
+	current_state.logic(delta)
+	
+	
 func set_state() -> Node:
 	return null
