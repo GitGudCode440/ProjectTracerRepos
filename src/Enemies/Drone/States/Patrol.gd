c794904d0d9f2266aa25e914036db6e018c15baa
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2022-02-16 16:49:59 +0500

    Update Patrol.gd

--- src/Enemies/Drone/States/Patrol.gd
+++ src/Enemies/Drone/States/Patrol.gd
@@ -1,4 +1,9 @@
 extends State
 
-var currentState = null
+func enter() -> void:
+	player.direction = 1
+
+func physics_logic(delta) -> void:
+	player.velocity.x = player.direction * player.speed
+
 
