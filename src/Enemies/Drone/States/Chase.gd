387f95d4b45357eab694a3fe91979cada7e4a879
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2022-02-19 23:01:17 +0500

    Queue freeing rays, moved chase code, and added shooting function.
    Queue freeing rays on entry, moved chase code to a seperate function, and added shooting function.

--- src/Enemies/Drone/States/Chase.gd
+++ src/Enemies/Drone/States/Chase.gd
@@ -2,18 +2,38 @@ extends State
 
 var directionToPlayer : Vector2 = Vector2.ZERO
 var chaseSpeed : int = 300
+var enemyLaser : Resource = preload("res://src/Bullet/EnemyBullet/EnemyBullet.tscn")
 
 func enter() -> void:
+	
+	
+	drone.ledgeDetector.queue_free()
+	drone.fieldOfView.queue_free()
 	
 	drone.collider.queue_free()
 
 
 func logic(delta) -> void:
 	
+	# Assigning a vector which gives us direction and ditstance of the player,
+	# in relation to itself.
 	directionToPlayer = (player.global_position - drone.global_position)
 	
+	shoot()
 	
 func physics_logic(delta) -> void:
+	
+	chase()
+	
+func shoot() -> void:
+	
+	var instance = enemyLaser.instance()
+	
+	drone.add_child(instance)
+	instance.set_direction(directionToPlayer.normalized())
+	
+func chase() -> void:
+	
 	
 	if directionToPlayer.length() < 200:
 		drone.velocity = lerp(drone.velocity, Vector2.ZERO, 0.05)
@@ -21,5 +41,5 @@ func physics_logic(delta) -> void:
 		drone.velocity = directionToPlayer.normalized() * chaseSpeed
 	
 	
-	print(drone.global_position)
+	
 	
