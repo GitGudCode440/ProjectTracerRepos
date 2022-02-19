19e7e00ee550b7bacf626e868b87603383d52127
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2022-02-19 23:31:39 +0500

    Created Turrret.gd and defined its behaviour

---
+++ src/Enemies/Turret/Turret.gd
@@ -0,0 +1,41 @@
+extends StaticBody2D
+
+enum states {IDLE, SHOOT}
+
+var currentState : int 
+
+var enemyLaser : Resource = preload("res://src/Bullet/EnemyBullet/EnemyBullet.tscn")
+
+onready var player : KinematicBody2D = get_tree().get_root().find_node("Player", true, false)
+var directionToPlayer : Vector2 = (player.global_position - global_position)
+
+
+func _ready():
+	currentState = states.IDLE
+	
+func _process(delta):
+	
+	if currentState == states.SHOOT:
+		shoot()
+	
+
+
+func _on_Area2D_body_entered(body):
+	if body == player:
+		currentState = states.SHOOT
+
+func _on_Area2D_body_exited(body):
+	
+	if body == player:
+		currentState = states.IDLE
+	
+	
+	
+func shoot() -> void:
+	
+	var instance = enemyLaser
+	
+	add_child(instance)
+	
+	instance.set_direction(directionToPlayer.normalized())
+	
