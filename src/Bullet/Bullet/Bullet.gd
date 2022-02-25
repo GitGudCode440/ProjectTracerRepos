acb748307e150cf3ad2ef522ccb2deaf042914a3
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2022-02-25 22:36:12 +0500

    Defined Area2D signal code which is to queue free on detection with player

--- src/Bullet/Bullet/Bullet.gd
+++ src/Bullet/Bullet/Bullet.gd
@@ -26,3 +26,9 @@ func set_direction(_direction) -> void:
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
 	
+	
+
+func _on_Bullet_body_entered(body):
+	
+	if body.name == "Player":
+		queue_free()
