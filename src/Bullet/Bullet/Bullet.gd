0043f6768c42a490cc52f66062426173b9fa9c77
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2022-03-04 23:49:23 +0500

    Replaced the code on Bullet area detected
    Replaced the code on Bullet area detected signal in which there was a check of collision masks and queue freeing on being met. Now it has been replaced with a impletentation of reducing lives instead and has better code.

--- src/Bullet/Bullet/Bullet.gd
+++ src/Bullet/Bullet/Bullet.gd
@@ -29,6 +29,7 @@ func _on_VisibilityNotifier2D_screen_exited():
 	
 
 func _on_Bullet_body_entered(body):
+	print(body)
 	
-	if body.collision_mask != get_parent().collision_mask:
-		queue_free()
+	if body.has_method("take_damage"):
+		body.call_deferred("take_damage")
