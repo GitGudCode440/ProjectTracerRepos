cc92002cd89e15fae5e79957b4062f2c1587e9fb
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2022-03-24 23:32:36 +0500

    Implemeted a Run State of Player states
    Implememted a Run State of Player states which has a transition to only Idle State.

--- scripts/Player/States/Run.gd
+++ scripts/Player/States/Run.gd
@@ -1,2 +1,23 @@
 extends State
 
+var runAnimation : String = "run"
+
+func enter() -> void:
+	animation = runAnimation
+	.enter()
+	
+	if host.direction > 0:
+		animatedSprite.flip_h = false
+	else:
+		animatedSprite.flip_h = true
+	
+func physics_logic(delta):
+	host.velocity.x = host.direction * host.speed
+	
+
+func get_transition(delta):
+	
+	if host.direction == 0:
+		return states.IDLE
+	else:
+		return null
