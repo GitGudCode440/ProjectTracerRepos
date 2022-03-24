59c725f29394cb390718a2d83608187af6632f5e
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2022-03-24 23:31:47 +0500

    Implemented a Idle State of Player states
    Implememted a Idle State of Player states which has a transition to only Run State.

--- scripts/Player/States/Idle.gd
+++ scripts/Player/States/Idle.gd
@@ -1,1 +1,21 @@
-extends State 
+extends State
+
+var idleAnimation : String = "idle"
+ 
+func enter() -> void:
+	animation = idleAnimation
+	.enter()
+	
+	if host.direction > 0:
+		pass
+	
+	
+	host.velocity.x = host.direction * host.speed
+
+func get_transition(delta):
+	
+	if abs(host.direction) > 0:
+		return states.RUN
+	else:
+		return null
+	
