817ef7408c127189b9b0540cd208dc97dd38e84b
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2022-03-26 13:43:15 +0500

    Added a transition to Jump State in Run.gd

--- scripts/Player/States/Run.gd
+++ scripts/Player/States/Run.gd
@@ -6,18 +6,22 @@ func enter() -> void:
 	animation = runAnimation
 	.enter()
 	
+func physics_logic(delta) -> void:
+	host.velocity.x = host.direction * host.speed
+	
+func logic(delta) -> void:
+	
 	if host.direction > 0:
 		animatedSprite.flip_h = false
-	else:
+	elif host.direction < 0:
 		animatedSprite.flip_h = true
-	
-func physics_logic(delta):
-	host.velocity.x = host.direction * host.speed
 	
 
 func get_transition(delta):
 	
-	if host.direction == 0:
+	if Input.is_action_just_pressed("jump") and host.is_on_floor():
+		return states.JUMP
+	elif host.direction == 0:
 		return states.IDLE
 	else:
 		return null
