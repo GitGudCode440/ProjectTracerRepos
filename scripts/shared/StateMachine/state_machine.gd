97bf2f8f44ab1c3f63c3977f2163258410a5c920
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2022-04-03 17:19:26 +0500

    Added a pushdown automata feature to state_machine

--- scripts/shared/StateMachine/state_machine.gd
+++ scripts/shared/StateMachine/state_machine.gd
@@ -9,53 +9,59 @@ class_name StateMachine
 export (Dictionary) onready var states
 
 
-export (NodePath) var first_state
-var current_state : Node 
+export (NodePath) var firstState
+var previousState : NodePath
+
+var currentState : Node
 
 
 func enter(_host : KinematicBody2D,
 			_target : KinematicBody2D) -> void:
 	
 	
-	
-	current_state = get_node(first_state)
+	previousState = firstState
+	currentState = get_node(firstState)
 	
 	for child in get_children():
 		child.target = _target
 		child.host = _host
+		
 		child.states = self.states
+		child.previousState = self.previousState
 		
 	
 	
-	current_state.enter()
+	currentState.enter()
 	
 	pass
 
 func logic(delta) -> void:
-	current_state.logic(delta)
+	currentState.logic(delta)
 	set_state(delta)
 
 func physics_logic(delta) -> void:
-	current_state.physics_logic(delta)
+	currentState.physics_logic(delta)
 	
 	
 func on_collisions(delta) -> void:
-	current_state.on_collisions(delta)
+	currentState.on_collisions(delta)
 
 	
 func set_state(delta) -> void:
 	
 	var transition : NodePath
 	
-	if !current_state.get_transition(delta):
+	
+	if !currentState.get_transition(delta):
 		return
 	else:
-		transition  = current_state.get_transition(delta)
-	
-	if transition != null:
-		current_state = get_node(transition)
-		current_state.enter()
-	
+		transition  = currentState.get_transition(delta)
+		
+		previousState = get_path_to(currentState)
+		
+		currentState = get_node(transition)
+		currentState.enter()
+		
 	
 	
 	
