c4e7a365926d2f35163b0210d1930fffa57b6172
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2022-04-10 05:44:56 +0500

    Defined transitions of Jump state of Player.

--- scripts/Player/States/Jump.gd
+++ scripts/Player/States/Jump.gd
@@ -12,7 +12,19 @@ func enter() -> void:
 
 func get_transition(delta):
 	
-	if host.is_on_floor() and abs(host.direction) > 0:
+	var absHostDirection = abs(host.direction)
+	
+	if Input.is_action_just_pressed("shoot") and absHostDirection == 0:
+		return states.SHOOT   
+		
+	elif Input.is_action_just_pressed("shoot") and absHostDirection > 0:
+		return states.RUN_SHOOT
+		
+	elif host.is_on_floor() and absHostDirection > 0:
 		return states.RUN
-	elif host.is_on_floor() and host.direction == 0:
+		
+	elif host.is_on_floor() and absHostDirection == 0:
 		return states.IDLE
+		
+	else:
+		return null
