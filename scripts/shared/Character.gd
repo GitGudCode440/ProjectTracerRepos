167ecdf75e9bda51614ca14810ce7bc81d4b3188
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2022-04-10 05:19:15 +0500

    Created Character as a base script for Player and Drone

---
+++ scripts/shared/Character.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+class_name Character
+
+export(int) var lives
+export(int) var speed
+export(float) var gravity
+
+var velocity : Vector2 = Vector2.ZERO
+var direction : float
+
+onready var states : Node = $States
+onready var damageSound : AudioStreamPlayer = $DamageSound
+
+func take_damage() -> void:
+	lives -= 1
+	damageSound.play()
+
+func check_lives() -> void:
+	if lives == 0:
+		queue_free()
+		
