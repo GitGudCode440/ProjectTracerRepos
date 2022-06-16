81adf454314404f110228122a44e934b94d58c8e
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2022-06-16 23:11:03 +0500

    Added signals for communicating with Game parent in Level.gd
    Added signals for communicating with Game parent in Level.gd for displaying ScreenManager's screens relating to a specific condition.

--- scripts/shared/Level.gd
+++ scripts/shared/Level.gd
@@ -1,9 +1,29 @@
 extends Node2D
 
-onready var portal : Area2D 
+onready var player : KinematicBody2D = $Player
 
-func _process(delta):
+onready var carRed : Area2D = $CarRed
+
+onready var animationPlayer : AnimationPlayer = $AnimationPlayer
+
+
+signal show_game_over
+signal show_level_comp
+
+func _ready() -> void:
+	self.connect("show_game_over", get_parent(), "show_game_over")
+	self.connect("show_level_comp", get_parent(), "show_level_comp")
+	
+	pass
+
+func _process(delta) -> void:
 	pass
 	
+	
+	
+func _on_CarRed_body_entered(body) -> void:
+	emit_signal("show_level_comp")
+	
+	self.disconnect("show_level_comp", get_parent(), "show_level_comp")
 
 	
