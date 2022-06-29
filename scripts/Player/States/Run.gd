14dee7cc6a8b77e4cda9a59f414361e0b0bfc11e
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2022-06-29 22:07:31 +0500

    Update Run.gd

--- scripts/Player/States/Run.gd
+++ scripts/Player/States/Run.gd
@@ -16,15 +16,18 @@ func logic(delta) -> void:
 	elif host.direction < 0:
 		host.animatedSprite.flip_h = true
 	
+	
 
 func get_transition(delta):
+	
+	
 	
 	if Input.is_action_just_pressed("jump") and host.is_on_floor():
 		return states.JUMP
 		
 	elif host.direction == 0:
 		return states.IDLE
-		
+	
 	elif Input.is_action_pressed("shoot"):
 		return states.RUN_SHOOT
 		
