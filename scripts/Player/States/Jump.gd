553f5ef4a315ddebbfa41cfd17005a5089c4976d
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2023-03-09 09:59:17 +0500

    Added jump buffer to Jump state

--- scripts/Player/States/Jump.gd
+++ scripts/Player/States/Jump.gd
@@ -2,17 +2,39 @@ extends Run
 
 var jumpAnimation : String = "jump"
 
+var jumpBufferTime = 0.95
+var jumpBufferTimer
+
+var isJumpBufferPressed = false
+
 
 func enter() -> void:
+	jumpBufferTimer = jumpBufferTime
+	isJumpBufferPressed = false
+	
+	
 	host.animatedSprite.play(jumpAnimation)
-	
-	
 	host.velocity.y = -host.jumpSpeed
 
-
+func logic(delta):
+	jumpBufferTimer -= 1.0 * delta
+	jumpBufferTimer = max(jumpBufferTimer, 0)
+	
+	if jumpBufferTimer == 0.0:
+		if Input.is_action_just_pressed("jump"):
+			isJumpBufferPressed = true
+	
+	
+	
 func get_transition(delta):
 	
 	var absHostDirection = abs(host.direction)
+	
+	
+	if isJumpBufferPressed and host.is_on_floor():
+		return states.JUMP
+	
+	
 	
 	if Input.is_action_just_pressed("shoot") and absHostDirection == 0:
 		return states.SHOOT   
@@ -25,6 +47,5 @@ func get_transition(delta):
 		
 	elif host.is_on_floor() and absHostDirection == 0:
 		return states.IDLE
-		
-	else:
-		return null
+	
+
