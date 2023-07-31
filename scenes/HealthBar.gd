4a2bf144beb4300f127966e20ed8e7184480ff93
M. Mahad Hashmi <m.mahadhashmi@gmail.com>
2023-07-31 19:58:22 +0500

    Added a health UI for displaying player's health.
    Added health UI for displaying player's health.

---
+++ scenes/HealthBar.gd
@@ -0,0 +1,44 @@
+extends TextureProgress
+
+var health : int
+onready var healthText := get_parent().get_node("HealthText")
+
+onready var player : KinematicBody2D = get_tree().get_root().find_node("Player", true, false)
+ 
+var tween : SceneTreeTween
+
+
+func _ready():
+	
+	
+	healthText.show()
+	show()
+	
+	kill_tween()
+	
+	health = player.health
+	max_value = health
+	value = health
+
+func on_playerHealth_damage(_damage):
+	health -= _damage
+	
+	kill_tween()
+	
+	tween = get_tree().create_tween().set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_OUT)
+	tween.tween_property(self, "value", health, 3.0)
+
+
+func _process(delta):
+	if health <= 0:
+		hide()
+		healthText.hide()
+
+func reset_health():
+	_ready()
+
+func kill_tween():
+	if tween:
+		if tween.is_running():
+			tween.kill()
+
