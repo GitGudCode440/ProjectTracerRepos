3a0fda38fa70b11204eb1ee90da810e69a44c724
M. Mahad Hashmi <m.mahadhashmi@gmail.com>
2023-08-02 16:17:01 +0500

    Removed infinite score glitch when it comes to SecretArea

--- scenes/levels/Level3.gd
+++ scenes/levels/Level3.gd
@@ -1,5 +1,8 @@
 extends "res://scripts/shared/Level.gd"
 
+onready var secretArea = $SecretArea
 
 func _on_SecretArea_body_entered(body):
 	get_tree().call_group("ScoreText", "gain_score", 500)
+	secretArea.disconnect("body_entered", self, "_on_SecretArea_body_entered")
+	secretArea.queue_free()
